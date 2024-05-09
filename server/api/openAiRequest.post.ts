ff4f5759b17a407ce989b8cc0ddb2815a33b286b
DanielL <d.luchin@gmail.com>
2024-05-09 22:19:57 +0200

    fix

--- server/api/openAiRequest.post.ts
+++ server/api/openAiRequest.post.ts
@@ -1,44 +1,50 @@
-import fs from "fs";
-import { OpenAI } from "langchain/llms/openai";
-import { RetrievalQAChain } from 'langchain/chains';
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
-import { ChatRequest } from "~/types/request";
-import { VECTOR_STORE_PATH } from "../consts/paths";
-import { sleep } from "../utils/global.utils";
+import fs from 'fs';
+import { OpenAI } from 'langchain/llms/openai';
 
+import { RetrievalQAChain } from 'langchain/chains';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { HNSWLib } from 'langchain/vectorstores/hnswlib';
+import { ChatRequest } from '~/types/request';
+import { VECTOR_STORE_PATH } from '../consts/paths';
+import { sleep } from '../utils/global.utils';
 
 export default defineEventHandler(async (event) => {
+  if (useRuntimeConfig().public.devMode) {
+    await sleep(1000);
+    return { message: 'DEBUG MESSAGE!' };
+  }
 
-    if (useRuntimeConfig().public.devMode) {
-        await sleep(1000);
-        return { message: "DEBUG MESSAGE!" }
+  try {
+    const model = new OpenAI({
+      modelName: 'gpt-3.5-turbo',
+      temperature: 0.1,
+    });
+
+    const { question } = await readBody<ChatRequest>(event);
+    console.log('🚀 ~ defineEventHandler ~ question:', question);
+
+    let vectorStore;
+    if (fs.existsSync(VECTOR_STORE_PATH)) {
+      vectorStore = await HNSWLib.load(
+        VECTOR_STORE_PATH,
+        new OpenAIEmbeddings()
+      );
+    } else {
+      throw 'Vector store does not exist. Try calling api/ingest first';
     }
 
-    try {
-        const model = new OpenAI({});
+    const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
 
-        const { question } = await readBody<ChatRequest>(event);
+    const res = await chain.call({
+      query: question,
+    });
 
-        let vectorStore;
-        if (fs.existsSync(VECTOR_STORE_PATH)) {
-            vectorStore = await HNSWLib.load(VECTOR_STORE_PATH, new OpenAIEmbeddings());
-        } else {
-            throw "Vector store does not exist. Try calling api/ingest first"
-        }
+    return { message: res.text };
+  } catch (error) {
+    // @ts-ignore unknown type
+    console.error(error.response);
 
-        const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
-
-        const res = await chain.call({
-            query: question,
-        });
-
-        return { message: res.text }
-    } catch (error) {
-        // @ts-ignore unknown type
-        console.error(error.response);
-
-        // @ts-ignore unknown type
-        throw error.response;
-    }
-})
+    // @ts-ignore unknown type
+    throw error.response;
+  }
+});
--- server/ingest/index.ts
+++ server/ingest/index.ts
@@ -8,53 +8,58 @@ import { VECTOR_STORE_PATH, INPUT_DOCS_PATH } from '../consts/paths';
 import { sleep } from '../utils/global.utils';
 
 const ingest = async () => {
-    if (useRuntimeConfig().public.devMode) {
-        await sleep(1000);
+  // if (useRuntimeConfig().public.devMode) {
+  //     await sleep(1000);
 
-        console.log({
-            status: 200,
-            message: "Ingest complete! - DEBUG",
-            error: null
-        })
+  //     console.log({
+  //         status: 200,
+  //         message: "Ingest complete! - DEBUG",
+  //         error: null
+  //     })
+  // }
+
+  try {
+    if (fs.existsSync(VECTOR_STORE_PATH)) {
+      console.log('Vector already exists exists');
+      console.log({
+        status: 200,
+        message: 'Vector already exists!',
+        error: null,
+      });
     }
+    console.log('Creating new Vector');
 
-    try {
-        if (fs.existsSync(VECTOR_STORE_PATH)) {
-            console.log("Vector already exists exists")
-            console.log({
-                status: 200,
-                message: "Vector already exists!",
-                error: null
-            })
-        }
-        console.log("Creating new Vector")
+    const directoryLoader = new DirectoryLoader(INPUT_DOCS_PATH, {
+      '.pdf': (path) => new CustomPDFLoader(path),
+    });
+    const rawDocs = await directoryLoader.load();
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
 
-        const directoryLoader = new DirectoryLoader(INPUT_DOCS_PATH, {
-            '.pdf': (path) => new CustomPDFLoader(path),
-        });
-        const rawDocs = await directoryLoader.load();
-        const textSplitter = new RecursiveCharacterTextSplitter({
-            chunkSize: 1000,
-            chunkOverlap: 200,
-        });
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    const vectorStore = await HNSWLib.fromDocuments(
+      docs,
+      new OpenAIEmbeddings({
+        openAIApiKey: '',
+      })
+    );
+    await vectorStore.save(VECTOR_STORE_PATH);
 
-        const docs = await textSplitter.splitDocuments(rawDocs);
-        const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-        await vectorStore.save(VECTOR_STORE_PATH);
-
-        console.log({
-            status: 200,
-            message: "Ingest complete!",
-            error: null
-        })
-    } catch (error) {
-        console.error("Error", error)
-        console.log({
-            status: 501,
-            message: "Error!",
-            error
-        })
-    }
-}
+    console.log({
+      status: 200,
+      message: 'Ingest complete!',
+      error: null,
+    });
+  } catch (error) {
+    console.error('Error', error);
+    console.log({
+      status: 501,
+      message: 'Error!',
+      error,
+    });
+  }
+};
 
 ingest();
